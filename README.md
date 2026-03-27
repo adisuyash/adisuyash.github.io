35a8411f7b2e0172335b508a31d088804d1512e8
adisuyash <adityasuyashgupta@gmail.com>
2026-03-27 06:31:33 +0530

    feat: add Google Analytics tracking to layout

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio Website
+
+`pnpm run deploy` - generates a static export of the Next.js code into static into gh-pages
--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,21 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <head>
+        {/* --- Google Analytics --- */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-B4P1N54GZQ"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-B4P1N54GZQ');
+      `,
+          }}
+        />
         <link
           rel="alternate"
           type="application/rss+xml"
